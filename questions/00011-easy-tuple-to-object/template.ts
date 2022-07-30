a790333de915f4cb3d7916aca48087b5e5a351e0
Sayaka Koyama <61900280+wannyanland@users.noreply.github.com>
2022-07-30 23:23:26 +0900

    20220730

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,5 @@
-type TupleToObject<T extends readonly any[]> = any
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type TupleToObject<T extends readonly(string | number)[]> = { [value in T[number]]: value }
+
+type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
